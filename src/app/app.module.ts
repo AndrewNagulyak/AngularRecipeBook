15f8aa1787ba9f97d67a0434a9454e522ed312a6
Andrew Naholik <andriinahyliak58@gmail.com>
2019-06-18 14:26:45 +0300

    dropdown directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { DropdownDirective } from './shared/Dropdown.directive';
 
 @NgModule({
    declarations: [
@@ -19,7 +20,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
       RecipeListComponent,
       RecipeDetailComponent,
       RecipesComponent,
-      ShoppingEditComponent
+      ShoppingEditComponent,
+      DropdownDirective
    ],
    imports: [
       BrowserModule
